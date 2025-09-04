d6bfc6124324b69ccc0d0633e4014a83809c8ec1
ElkinC21 <elkin.leo.cruz21@gmail.com>
2025-09-04 20:32:09 +0200

    Prueba#2

---
+++ src/prueba/pkg2/Archivo.java
@@ -0,0 +1,103 @@
+
+package prueba.pkg2;
+
+
+import java.io.File;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Archivo {
+private File archivo;
+
+
+public Archivo() throws IOException{
+archivo=new File("Tareas.txt");
+archivo.createNewFile();
+}
+public void agregarTarea(String tarea) throws IOException{
+    try (FileWriter archivoescribir = new FileWriter(archivo,true)) {
+        archivoescribir.write("[]"+tarea);
+        archivoescribir.flush();
+    }
+
+}
+public void mostrartareas() throws IOException {
+    FileReader fr = new FileReader(archivo);
+
+    int c;
+    String linea = "";
+    int contador = 1;
+
+    while ((c = fr.read()) != -1) {
+        char letra = (char) c;
+
+        if (letra == '\n') {
+            System.out.println(contador + ". " + linea);
+            linea = "";
+            contador++;
+        } else {
+            linea += letra;
+        }
+    }
+    
+    if (!linea.isEmpty()) {
+        System.out.println(contador + ". " + linea);
+    }
+
+    fr.close();
+}
+
+
+public void Completartarea(int numtarea) throws IOException {
+    FileReader archivoleer = new FileReader(archivo);
+
+    int c;
+    String linea = "";
+    String todo = "";
+    int contador = 1;
+    boolean existe = false;
+
+    while ((c = archivoleer.read()) != -1) {
+        char letra = (char) c;
+
+        if (letra == '\n') {
+            if (contador == numtarea) {
+                linea = "[✔]" + linea.substring(2);
+                existe = true;
+            }
+            todo += linea + "\n";
+            linea = "";
+            contador++;
+        } else {
+            linea += letra;
+        }
+    }
+
+    
+    if (!linea.isEmpty()) {
+        if (contador == numtarea) {
+            linea = "[✔]" + linea.substring(2);
+            existe = true;
+        }
+        todo += linea + "\n";
+    }
+
+    archivoleer.close();
+
+    if (!existe) {
+        System.out.println("No se encontro la tarea numero " + numtarea);
+        return; 
+    }
+
+    
+    FileWriter fw = new FileWriter(archivo);
+    fw.write(todo);
+    fw.close();
+}
+
+
+
+}
---
+++ src/prueba/pkg2/Main.java
@@ -0,0 +1,76 @@
+
+package prueba.pkg2;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner lea = new Scanner(System.in);
+        Archivo archivo = null;
+
+        try {
+            archivo = new Archivo();
+        } catch (IOException e) {
+            System.out.println("Error al crear el archivo: " + e.getMessage());
+            return;
+        }
+
+        while (true) {
+            System.out.println("\nGestor de tareas\n=============");
+            System.out.println("1.Agregar tarea:");
+            System.out.println("2.Mostrar tarea");
+            System.out.println("3.Completar tarea");
+            System.out.println("4.Salir");
+            System.out.print("Seleccione una opcion: ");
+            int opcion = lea.nextInt();
+            lea.nextLine(); 
+
+            switch (opcion) {
+
+                case 1:
+                    System.out.print("Ingrese tarea a agregar: ");
+                    String nuevatarea = lea.nextLine();
+                    try {
+                        archivo.agregarTarea(nuevatarea + "\n");
+                        System.out.println("Tarea agregada");
+                    } catch (IOException e) {
+                        System.out.println("No pudo agregarse la tarea " + e.getMessage());
+                    }
+                    break;
+
+                case 2:
+                    try {
+                        archivo.mostrartareas();
+                    } catch (IOException e) {
+                        System.out.println("Erro al mostrar las tareas " + e.getMessage());
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Numero de tarea a completar: ");
+                    int numtarea = lea.nextInt();
+                    lea.nextLine(); 
+                    try {
+                        archivo.Completartarea(numtarea);
+                        System.out.println("Tarea completada");
+                    } catch (IOException e) {
+                        System.out.println("Error al completar la tarea " + e.getMessage());
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+                    break;
+
+                default:
+                    System.out.println("Ingrese una opcion valida");
+                    break;
+            }
+            if (opcion == 4) {
+                break;
+            }
+        }
+    }
+}
